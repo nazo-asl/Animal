be0e63618f10059246673a1ebfc39e09b04a9b44
livias15 <livias15@10.30.4.85>
2018-01-11 09:28:50 +0000

    Created Dog Animal

---
+++ src/Dog.java
@@ -0,0 +1,9 @@
+
+public class Dog extends Animal {
+
+	public Dog(String sound, boolean hasWings, int numLegs) {
+		super(sound, hasWings, numLegs, sound);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
