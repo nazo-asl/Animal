5656245ff5bbbb54f20fbdbcd339e5b83d52a959
kentc14 <kentc14@10.30.4.148>
2018-01-11 08:35:10 +0000

    commit

---
+++ src/Animal.java
@@ -0,0 +1,21 @@
+/***
+ * Represents the concept of an animal.
+ * 
+ * @author kentc14
+ *
+ */
+public class Animal {
+
+	String sound;
+	boolean hasWings;
+	int numLegs;
+	
+	/**
+	 * Pretend to speak
+	 * 
+	 * @return the animal's sound
+	 */
+	public String speak() {
+		return sound;
+	}
+}
