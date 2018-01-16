6e7fb355838b948e928d18ef8dfdaa43d77a9514
vannn <vannn@10.31.4.86>
2018-01-16 12:36:21 +0000

    included Bear

---
+++ src/Bear.java
@@ -0,0 +1,27 @@
+
+public class Bear extends Animal {
+
+	public Bear(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getSound() {
+		// TODO Auto-generated method stub
+		return "GRRROAR";
+	}
+
+	@Override
+	public boolean hasWings() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNumLegs() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+}
