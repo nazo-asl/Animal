1c7e59c788c8de5cdad1301c45193030bb7c89f5
momotheritz <momotheritz@10.31.4.156>
2018-01-16 12:36:32 +0000

    included Kangeroo

---
+++ Kangeroo.java
@@ -0,0 +1,27 @@
+
+public class Kangeroo extends Animal {
+
+	public Kangeroo(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getSound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasWings() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNumLegs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
