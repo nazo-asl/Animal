874ed46ab63a5bd83df71cbfe7df4ed952fa8d22
DomA-asl <albertsdo@10.31.4.15>
2018-01-16 12:36:19 +0000

    included raccoon

---
+++ src/Raccoon.java
@@ -0,0 +1,27 @@
+
+public class Raccoon extends Animal {
+
+	public Raccoon(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getSound() {
+		// TODO Auto-generated method stub
+		return "Scrrrrr";
+	}
+
+	@Override
+	public boolean hasWings() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNumLegs() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+}
